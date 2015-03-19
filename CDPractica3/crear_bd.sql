9f943be40afb9a943c49692f5197afc78c63b73b
martin alonso <m13alonso@gmail.com>
2015-03-19 16:57:35 +0100

    Script creacion BD

---
+++ CDPractica3/crear_bd.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE meteochatbd;
+USE meteochatbd;
+
+CREATE USER 'adminmeteo'@'localhost' IDENTIFIED BY '1234';
+GRANT ALL PRIVILEGES ON 'meteochat' . * TO 'adminmeteo'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE TABLE clients (
+   name VARCHAR(64) NOT NULL,
+   email VARCHAR(64) NOT NULL,
+   date DATETIME NOT NULL,
+   PRIMARY KEY (nombre)
+);
+
+CREATE TABLE friends (
+   client1 VARCHAR(64) NOT NULL,
+   client2 VARCHAR(64) NOT NULL,
+   PRIMARY (client1, client2),
+   FOREIGN KEY (client1) REFERENCES clients(name) ON UPDATE CASCADE ON DELETE CASCADE,
+   FOREIGN KEY (client2) REFERENCES clients(name) ON UPDATE CASCADE ON DELETE CASCADE
+);
