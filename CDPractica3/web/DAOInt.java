e91463340e4386ba6ed4e708be4621272a926a09
martin alonso <m13alonso@gmail.com>
2015-03-23 18:11:21 +0100

    ..

--- CDPractica3/web/DAOInt.java
+++ CDPractica3/web/DAOInt.java
@@ -3,9 +3,11 @@ import java.util.ArrayList;
 
 public interface DAOInt {
 
+   public ArrayList<Client> getFriends(Client client);
    public ArrayList<Client> getFriends(String name);
-   public void newClient();
-   public void removeClient();
-   public void changeClientPass(String name);
+   public void newClient(Client client);
+   public void removeClient(Client client);
+   public void removeClient(String name);
+   public void changeClientPass(String name, String pass);
 
 }
