0682d15054570cded9822b768098868823295cfc
Pablo Varela <pablovarela182@gmail.com>
2015-03-23 18:28:20 +0100

    Ordenaaaao

---
+++ CDPractica3/web/WEB-INF/classes/controller/Controller.java
@@ -0,0 +1,13 @@
+
+package controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class Controller extends HttpServlet {
+
+    public void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+
+}
