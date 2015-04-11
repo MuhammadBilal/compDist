96a5c928ce67e654157c09966628759562cc4a1f
Pablo Varela <pablovarela182@gmail.com>
2015-04-11 15:52:19 +0200

    El script del server mata al server anterior

--- CDPractica3/server/ejecutar-server.sh
+++ CDPractica3/server/ejecutar-server.sh
@@ -1,6 +1,15 @@
 cd server
 
-killall java > /dev/null
+last_pid=0
+
+if [ -f .server_pid ]; then
+	last_pid=`cat .server_pid`
+fi
+
+if [ $last_pid -ne 0 ]; then
+	kill $last_pid
+fi
 
 java Server &
 
+echo $! > .server_pid
